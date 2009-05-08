da04c0ebb69ff4f610d949eebb694abcc73735d8
Scott Woods <scott@westarete.com>
2009-05-08 16:53:14 -0400

    This might work, but it's still taking too long.

---
+++ 3/two.rb
@@ -0,0 +1,37 @@
+class Prime
+  def self.downfrom(max)
+    max.downto(2) do |i|
+      yield i if i.prime?
+    end
+  end
+end
+
+module Factoring
+  def is_factor_of?(number)
+    self % number == 0
+  end
+  
+  def prime?
+    2.upto(self-1) do |i|
+      if self % i == 0
+        return false
+      end
+    end
+    true
+  end
+end
+
+class Fixnum
+  include Factoring
+end
+
+class Bignum
+  include Factoring
+end
+
+NUMBER = 600851475143
+
+Prime.downfrom(NUMBER/2) do |prime|
+  greatest_prime_factor = prime if prime.is_factor_of?(NUMBER)
+end
+puts greatest_prime_factor
